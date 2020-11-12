8379290f1a357ade428c7e553ec7a17b0d1ec8af
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-12 19:15:50 +0100

    Add script

---
+++ Covid19DataParser/Covid19DataParser/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+const int DATE_COLUMN_INDEX = 1;
+const int CITY_CODE__COLUMN_INDEX = 10;
+const int CITY_NAME__COLUMN_INDEX = 11;
+const int NEW_CASES_COLUMN_INDEX = 12;
+const int CURENT_CASES_COLUMN_INDEX = 13;
+
+var filename = @"C:\Work\VSB\Covid-19-cz\obce\obec.csv";
+
+FileStream fileStream = new(filename, FileMode.Open, FileAccess.Read);
+Dictionary<string,City> cities = new();
+
+using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+{
+    string line;
+
+    while ((line = streamReader.ReadLine()) != null)
+    {
+        if (line == "" || line.StartsWith("den;"))
+            continue;
+
+        var values = line.Split(";");
+        var cityCode = values[CITY_CODE__COLUMN_INDEX];
+        var cityName = values[CITY_NAME__COLUMN_INDEX];
+
+        if (!cities.ContainsKey(cityCode))
+        {
+            cities.Add(cityCode, new City { Code = cityCode, Name = cityName });
+        }
+
+        // add entry 
+        var day = values[DATE_COLUMN_INDEX];
+        Int32.TryParse(values[NEW_CASES_COLUMN_INDEX], out var new_case);
+        Int32.TryParse(values[CURENT_CASES_COLUMN_INDEX], out var actual_cases);
+        cities[cityCode].days.Add(day, (new_case, actual_cases));
+    }
+
+    // Generate header line
+    DateTime curentDate = Convert.ToDateTime("2020-03-01");
+    DateTime enddate = Convert.ToDateTime("2020-11-02");
+
+    var linestoWrite = new List<string>();
+    var headerLine = "CityName;";
+
+    while (curentDate < enddate)
+    {
+        headerLine += curentDate.ToString("yyyy-MM-dd") + ";";
+        curentDate = curentDate.AddDays(1);
+    }
+    linestoWrite.Add(headerLine);
+
+    // Generate lines to write
+
+    curentDate = Convert.ToDateTime("2020-03-01");
+    enddate = Convert.ToDateTime("2020-11-02");
+
+    foreach(var kvpCity in cities.ToList())
+    {
+        var lineToWrite = kvpCity.Key+";" + kvpCity.Value.Name + ";";
+        while (curentDate < enddate)
+        {
+            var daystr = curentDate.ToString("yyyy-MM-dd");
+
+            if(kvpCity.Value.days.ContainsKey(daystr))
+            {
+                lineToWrite += $"{kvpCity.Value.days[daystr].Item1};{kvpCity.Value.days[daystr].Item2};";
+            }
+            else
+            {
+                lineToWrite += "NA,NA;"; 
+            }
+
+            curentDate = curentDate.AddDays(1);
+        }
+        // write and reset date
+        linestoWrite.Add(lineToWrite);
+        curentDate = Convert.ToDateTime("2020-03-01");
+    }
+
+    using (StreamWriter file = new StreamWriter(@"output.csv"))
+    {
+        foreach (var l in linestoWrite)
+        {
+            file.WriteLine(l);
+        }
+    }
+}
+
+public record City
+{
+    public string Code;
+    public string Name;
+    public Dictionary<string, (int, int)> days = new Dictionary<string, (int, int)>();
+};
